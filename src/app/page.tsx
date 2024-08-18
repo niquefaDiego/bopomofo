65ddb3efaf8e164e2812bfc966b28b0398d47f87
Diego Niquefa <niquefa.diego@gmail.com>
2024-08-17 20:45:48 -0700

    add boilerplate code

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import BopomofoKeybaord  from "./components/bopomofo-keyboard";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> Bopomofo Keyboard </h1>
+      <BopomofoKeybaord />
+    </main>
+  );
+}
